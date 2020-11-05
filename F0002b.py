6dfe173d7aef893c860c09d46f6e380c0880428f
Lopatovszki <55076178+Lopatovszki@users.noreply.github.com>
2020-11-05 21:55:26 +0100

    Add files via upload

---
+++ F0002b.py
@@ -0,0 +1,12 @@
+#Mi lesz kiírva a program kimenetének utolsó sorában?(tyúk)
+
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
